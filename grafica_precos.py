69455be1b1b41f3e632bda1ed0d126e81974c542
juniordefreitas <juniordefreitas1@gmail.com>
2025-04-17 17:20:32 -0300

    grafica

--- grafica_precos.py
+++ grafica_precos.py
@@ -6,7 +6,7 @@ import math
 
 tabelinha = []
 site = requests.get(
-    'https://www.futuraim.com.br/produto/cartao-de-visita-em-couche-brilho?id=4527#')
+    'https://www.futuraim.com.br/produto/cartao-de-visita-em-couche-brilho?id=4527')
 dados_pagina = BeautifulSoup(site.text, 'html.parser')
 
 # PROCURA TODOS DIVS DA PAGINA QUE CONTENHAM A CLASSE form-check
@@ -31,12 +31,29 @@ for div2 in preco:
     # print(texto_preco)
     lista_preco.append(texto_preco)
 # coloca os valores de texto_filtro e texto_preco em uma lista
+
+
+titulo = dados_pagina.find_all('div', class_="col-md-6")
+# print(titulo)
+for div3 in titulo:
+    # print(div3)
+    filtro4 = div3.find('p', class_="font-small")
+    if filtro4:
+       # print(filtro4)
+        texto_filtro2 = div3.get_text(strip=True)
+        parte = texto_filtro2[:88]
+
+        print(f'\n{parte}\n')
+
+
 for texto_filtro, texto_preco in zip(lista_qtd, lista_preco):
     # adiciona os valores na lista tabelinha
     tabelinha.append((texto_filtro, texto_preco))
     # print(tabela)
 for item in tabelinha:
     # replce revove o R$ e a virgula e atribui o ponto
-    venda = float(item[1].replace('R$', '').replace(',', '.'))
+    custo = float(item[1].replace('R$', '').replace(',', '.'))
+    venda = (custo*1.66)
     # imprime os valores da lista tabelinha
-    print(f'Custo= {item[0]} : {item[1]} \nPreço de venda: {item[0]} = R$ {venda * 1.66:.2f} ','\n')
+    print(
+        f'Custo= {item[0]} : R$ {custo:.2f} \nPreço de venda: {item[0]} = R$ {venda:.2f} ', '\n')
