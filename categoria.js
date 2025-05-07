813a34feec032a676ac1a07a4a6d26ad1a489e6a
juniordefreitas <juniordefreitas1@gmail.com>
2025-05-07 16:49:30 -0300

    subindo da ff

---
+++ categoria.js
@@ -0,0 +1,21 @@
+<script>    
+    alert("Categoria do atleta");
+    var nome = prompt("Digite o nome");
+    var idade = +prompt("Digite a idade");
+    var categoria;
+
+    switch (idade) {
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+            categoria="Infantil"
+            
+            break;
+    
+        default:
+            break;
+    }
+</script>
